bd054eec2ee0627f8d33fc094a02329ed88005b6
Ritesh Kumar <rkritesh078@gmail.com>
2017-09-03 01:31:52 +0530

    feat(proptypes): implemented the plugin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,100 @@
-export default function(a, b) {
-  return a + b;
+import uniqBy from "lodash/uniqBy";
+import isEmpty from "lodash/isEmpty";
+
+/**
+ * This is a babel plugin that converts an object to PropTypes
+ * @param t
+ * @returns {{visitor: {ArrayExpression: (function(*)), NullLiteral: (function(*)), StringLiteral: (function(*)), NumericLiteral: (function(*)), ArrowFunctionExpression: (function(*)), FunctionExpression: (function(*)), BooleanLiteral: (function(*)), Program: (function(*, *)), ObjectExpression: (function(*))}}}
+ */
+export default function({ types: t }) {
+  return {
+    visitor: {
+      ArrayExpression(path) {
+        if (t.isCallExpression(path.parent)) return;
+        let uniqueElements = uniqBy(path.node.elements, "type");
+
+        if (t.isObjectExpression(uniqueElements[0])) {
+          uniqueElements = [
+            t.callExpression(
+              t.memberExpression(
+                t.identifier("PropTypes"),
+                t.identifier("shape")
+              ),
+              uniqueElements
+            )
+          ];
+        }
+
+        if (
+          (!isEmpty(uniqueElements) && uniqueElements.length === 1) ||
+          t.isCallExpression(uniqueElements)
+        ) {
+          path.replaceWith(
+            t.callExpression(
+              t.memberExpression(
+                t.identifier("PropTypes"),
+                t.identifier("arrayOf")
+              ),
+              uniqueElements
+            )
+          );
+        } else {
+          path.replaceWith(t.identifier("PropTypes.array"));
+        }
+      },
+
+      NullLiteral(path) {
+        path.replaceWith(t.identifier("PropTypes.any"));
+      },
+
+      StringLiteral(path) {
+        if (path.parentKey === "key") return;
+        path.replaceWith(t.identifier(`PropTypes.string`));
+      },
+
+      NumericLiteral(path) {
+        path.replaceWith(t.identifier("PropTypes.number"));
+      },
+
+      ArrowFunctionExpression(path) {
+        path.replaceWith(t.identifier("PropTypes.func"));
+      },
+
+      FunctionExpression(path) {
+        path.replaceWith(t.identifier("PropTypes.func"));
+      },
+
+      BooleanLiteral(path) {
+        path.replaceWith(t.identifier("PropTypes.bool"));
+      },
+
+      Program(path) {
+        path.traverse({
+          enter(path) {
+            t.removeComments(path.node);
+          }
+        });
+      },
+
+      ObjectExpression(path) {
+        if (
+          t.isVariableDeclarator(path.parent) ||
+          t.isCallExpression(path.parent)
+        )
+          return;
+
+        const elements = [path.node];
+
+        path.replaceWith(
+          t.callExpression(
+            t.memberExpression(
+              t.identifier("PropTypes"),
+              t.identifier("shape")
+            ),
+            elements
+          )
+        );
+      }
+    }
+  };
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,6 +1,54 @@
 import test from 'ava'
-import sum from '../src'
+import { transform } from 'babel-core'
+import plugin from '../src'
+
+const json = `{
+  "hello": [
+    {
+      "_id": "5988946e45e52d60b33a25c7",
+      "latitude": 50.087977,
+      "longitude": 72.167197,
+      "tags": [
+        "nulla",
+        "ullamco"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Robinson Woods"
+        },
+        {
+          "id": 1,
+          "name": "Lottie Hogan"
+        }
+      ]
+    },
+    {
+      "_id": "5988946ef6090217857d7b0f",
+      "latitude": 47.460772,
+      "longitude": 85.95137,
+      "tags": [
+        "aliqua",
+        "nulla"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Mamie Wyatt"
+        },
+        {
+          "id": 1,
+          "name": "Alejandra Mcdaniel"
+        }
+      ]
+    }
+  ]
+}`
 
 test('Add', (t) => {
-	t.is(sum(2,3), 5)
+	const {code} = transform(`const propTypes = ${json}`, {
+		plugins: [plugin]
+	})
+
+	t.snapshot(code)
 })
